2039338b1a7fd0183aa7af90f8563b27644914b3
9hodev <siaskkh4884@gmail.com>
2024-06-15 17:58:00 +0700

    changelog

--- libs/Database.ts
+++ libs/Database.ts
@@ -2,9 +2,7 @@ import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.NEXT_PUBLIC_MONGODB_URI as string;
 
-if (!MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
-}
+if (!MONGODB_URI) throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
 
 const options: any = {
   useNewUrlParser: true,
@@ -14,7 +12,7 @@ const options: any = {
 const connectDB = async () => {
   try {
     await mongoose.connect(MONGODB_URI, options);
-    console.log('MongoDB connected...');
+    console.log('MongoDB connected!');
   } catch (err: any) {
     console.error(err.message);
     process.exit(1);
