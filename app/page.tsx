f0a61e0886e6034b5b05a03826fe08bd93cd52b7
9hodev <siaskkh4884@gmail.com>
2024-06-20 01:43:15 +0700

    optimize

--- app/page.tsx
+++ app/page.tsx
@@ -142,7 +142,6 @@ export default function Home() {
                         </div>
                       </div>
                     </div>
-
                   </div>
                 ))}
               </div>
