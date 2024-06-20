b8d33482027ec4055be013521d3ae15263ab0865
variz-h264 <animesekai710@gmail.com>
2024-06-20 10:21:07 +0700

    fix products page

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -3,13 +3,19 @@ import Container from '@/components/interface/Container'
 export default function products() {
   return (
     <Container>
-      <div id="way" className="pt-[100px] pb-[100px] flex justify-center px-6 2xl:px-0">
-          <div className="w-full xl:w-[1280px]">
-            <div className="md:min-w-[450px]">
-                <h1 className="font-thai text-4xl md:text-6xl text-transparent bg-clip-text bg-gradient-to-r from-green-300 to-green-500 font-bold">All Product</h1>
-            </div>
+      <div className="pt-[100px] pb-[100px] flex justify-center px-6 2xl:px-0">
+        <div className="w-full xl:w-[1280px]">
+          <div className="md:min-w-[450px]">
+            <h1 className="font-thai text-4xl md:text-6xl text-transparent bg-clip-text bg-gradient-to-r from-green-300 to-green-500 font-bold">All Product</h1>
+          </div>
+
+          <div className="w-full text-white text-base md:text-xl">
+            <p>
+              <span className="font-eng text-red-400">Products not found</span>
+            </p>
           </div>
         </div>
+      </div>
     </Container>
   )
 }
