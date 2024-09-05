be6c948cfb36d87dcc157142cf0b5ceaaa3c7017
9hodev <siaskkh4884@gmail.com>
2024-09-05 17:50:10 +0700

    add desc

--- components/core/VerticalBar.tsx
+++ components/core/VerticalBar.tsx
@@ -3,21 +3,26 @@ import React from 'react'
 
 function VerticalBar() {
   return (
-    <div data-aos="fade-right" className="vt-container flex">
-        <div className="bar min-h-screen bg-[#27ff52] w-[20%] opacity-20 transition-all hover:opacity-[1] hover:scale-50 flex justify-center">
-            <h2 className='text-[#fff] [writing-mode:vertical-lr] text-8xl text-center'>"Code is poetry."</h2>
+    <div data-aos="fade-right" className="vt-container hidden md:visible md:flex mb-4">   
+        <div className="bar relative min-h-screen bg-[#27ff52] w-[20%] opacity-20 transition-all hover:opacity-[1] hover:scale-110 flex justify-center items-center">
+            <img className='object-cover absolute h-full brightness-50' src='https://images7.alphacoders.com/133/1337527.png'></img>
+            <h2 className='text-[#fff] text-4xl text-center z-20'>Code is poetry</h2>
         </div>
-        <div className="bar min-h-screen bg-[#27ff52] w-[20%] opacity-20 transition-all hover:opacity-[1] hover:scale-50 flex justify-center">
-            <h2 className='text-[#fff] [writing-mode:vertical-lr] text-8xl text-center'>"Innovate or evaporate."</h2>
+        <div className="bar relative min-h-screen bg-[#27ff52] w-[20%] opacity-20 transition-all hover:opacity-[1] hover:scale-110 flex justify-center items-center">
+            <img className='object-cover absolute h-full brightness-50' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ747wjpkNihDgeKW_Fm7H7NCS8XNz1gmXdTw&s'></img>
+            <h2 className='text-[#fff] text-4xl text-center z-20'>Eat. Sleep. <br></br> Code. Repeat.</h2>
         </div>
-        <div className="bar min-h-screen bg-[#27ff52] w-[20%] opacity-20 transition-all hover:opacity-[1] hover:scale-50 flex justify-center">
-            <h2 className='text-[#fff] [writing-mode:vertical-lr] text-8xl text-center'>"Think outside the binary."</h2>
+        <div className="bar relative min-h-screen bg-[#27ff52] w-[20%] opacity-20 transition-all hover:opacity-[1] hover:scale-110 flex justify-center items-center">
+            <img className='object-cover absolute h-full brightness-50' src='https://www.prachachat.net/wp-content/uploads/2021/04/23-1%E0%B9%81%E0%B8%AB%E0%B8%A5%E0%B9%88%E0%B8%87%E0%B9%80%E0%B8%AD%E0%B8%A3%E0%B8%B2%E0%B8%A7%E0%B8%B1%E0%B8%93-scaled.jpg'></img>
+            <h2 className='text-[#fff] text-4xl text-center z-20'>Data is the new oil</h2>
         </div>
-        <div className="bar min-h-screen bg-[#27ff52] w-[20%] opacity-20 transition-all hover:opacity-[1] hover:scale-50 flex justify-center">
-            <h2 className='text-[#fff] [writing-mode:vertical-lr] text-8xl text-center'>"Disrupt the status quo."</h2>
+        <div className="bar relative min-h-screen bg-[#27ff52] w-[20%] opacity-20 transition-all hover:opacity-[1] hover:scale-110 flex justify-center items-center">
+            <img className='object-cover absolute h-full brightness-50' src='https://i.pinimg.com/originals/cb/1a/0d/cb1a0d467344426d6b7588a3a1eb7f52.jpg'></img>
+            <h2 className='text-[#fff] text-4xl text-center z-20'>Fire! <br></br> git commit <br></br> git push <br></br> exit</h2>
         </div>
-        <div className="bar min-h-screen bg-[#27ff52] w-[20%] opacity-20 transition-all hover:opacity-[1] hover:scale-50 flex justify-center">
-            <h2 className='text-[#fff] [writing-mode:vertical-lr] text-8xl text-center'>"Technology is the new literacy."</h2>
+        <div className="bar relative min-h-screen bg-[#27ff52] w-[20%] opacity-20 transition-all hover:opacity-[1] hover:scale-110 flex justify-center items-center">
+            <img className='object-cover absolute h-full brightness-50' src='https://t3.ftcdn.net/jpg/05/89/67/22/360_F_589672270_s3dgY4fE2XqesMKB8gyqFyCBPIgbeV2Y.jpg'></img>
+            <h2 className='text-[#fff] text-4xl text-center z-20'>Clean Like Green World</h2>
         </div>
     </div>
   )
